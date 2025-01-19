434825df8c39d7ff08ffbeebb6c239d37c0c19c0
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-01-19 19:28:49 -0300

    vercel.json added

--- src/app/Util/File.php
+++ src/app/Util/File.php
@@ -7,7 +7,7 @@ class File {
     public static function save(string $tmp_name, string $save_name, string $save_directory = __DIR__ . "/../Storage/uploads/products/") {
         $save_directory = $save_directory . explode(".", $save_name)[0];
 
-        mkdir($save_directory);
+        mkdir($save_directory, 0777, true);
         move_uploaded_file($tmp_name, $save_directory . "/" . $save_name);
     }
 
